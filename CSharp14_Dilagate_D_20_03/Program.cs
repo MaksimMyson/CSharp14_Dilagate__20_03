ed430c281aeafecf1a4843ee7c5c62865191d465
MaksimMyson <maksimmyson33@gmail.com>
2024-03-20 19:12:31 +0200

    ex1 was done

--- CSharp14_Dilagate_D_20_03/Program.cs
+++ CSharp14_Dilagate_D_20_03/Program.cs
@@ -1,10 +1,36 @@
 ﻿namespace CSharp14_Dilagate_D_20_03
 {
-    internal class Program
+    public delegate void MessageDelegate(string message);
+
+    public class MessageApp
     {
+        
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine($"Повідомлення: {message}");
+        }
+
+        
+        public static void SendMessage(MessageDelegate messageDelegate, string message)
+        {
+            messageDelegate(message);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;  
+            
+            MessageDelegate messageDelegate = new MessageDelegate(DisplayMessage);
+
+            
+            SendMessage(messageDelegate, "Це тестове повідомлення.");
+
+            
+            SendMessage(messageDelegate, "Це ще одне повідомлення.");
+
+            
+            messageDelegate = new MessageDelegate(Console.WriteLine);
+            SendMessage(messageDelegate, "Інше повідомлення з іншим методом виводу.");
         }
     }
 }
